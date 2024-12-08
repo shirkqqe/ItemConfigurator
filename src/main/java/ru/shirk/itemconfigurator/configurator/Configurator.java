fd3008f40b8761119706b1e11826ddd03a11db13
shirkqqe <l3shabs@mail.ru>
2024-12-08 20:42:20 +0300

    added javadoc

--- src/main/java/ru/shirk/itemconfigurator/configurator/Configurator.java
+++ src/main/java/ru/shirk/itemconfigurator/configurator/Configurator.java
@@ -10,21 +10,59 @@ import ru.shirk.itemconfigurator.configurator.items.CItem;
 import java.util.List;
 
 public interface Configurator {
+    /**
+     * @param section Configuration section where you need to write items.
+     * @param itemStacks Items that need to be saved to the configuration.
+     * @throws CException If writing fails.
+     */
     void writeItemsToConfiguration(@NonNull ConfigurationSection section, @NonNull ItemStack[] itemStacks) throws CException;
 
+    /**
+     * @param itemStack Item to be written to the configuration.
+     * @param section Configuration section where you need to write item.
+     * @throws CException If writing fails.
+     */
     void writeItemToConfiguration(@NonNull ItemStack itemStack, @NonNull ConfigurationSection section) throws CException;
 
+    /**
+     * @param inventory Inventory that needs to be saved to the configuration.
+     * @param key Key for saving inventory.
+     * @param title Inventory title
+     * @param section Configuration section where you need to write item.
+     * @throws CException If writing fails.
+     */
     void writeInventoryToConfiguration(@NonNull Inventory inventory, @NonNull String key, @NonNull String title, @NonNull ConfigurationSection section) throws CException;
 
+    /**
+     * @param section Configuration section from which to obtain items.
+     * @return Resulting list of items.
+     * @throws CException If reading failed or some important section was not found.
+     */
     @NonNull
     List<CItem> getItemsFromConfiguration(@NonNull ConfigurationSection section) throws CException;
 
+    /**
+     * @param section Configuration section from which to obtain item.
+     * @param path Path to get the item.
+     * @return CItem object.
+     * @throws CException If reading failed or some important section was not found.
+     */
     @NonNull
     CItem getItemFromConfiguration(@NonNull ConfigurationSection section, @NonNull String path) throws CException;
 
+    /**
+     * @param section Configuration section from which to get the inventory.
+     * @return CInventory object.
+     * @throws CException If reading failed or some important section was not found.
+     */
     @NonNull
     CInventory getInventoryFromConfiguration(@NonNull ConfigurationSection section) throws CException;
 
+    /**
+     * @param section Configuration section from which you can get a list of inventories.
+     * @return Resulting list of inventories.
+     * @throws CException If reading failed or some important section was not found.
+     */
     @NonNull
     List<CInventory> getInventoryListFromConfiguration(@NonNull ConfigurationSection section) throws CException;
 }
--- src/main/java/ru/shirk/itemconfigurator/configurator/Serializable.java
+++ src/main/java/ru/shirk/itemconfigurator/configurator/Serializable.java
@@ -4,7 +4,16 @@ import lombok.NonNull;
 import org.bukkit.configuration.ConfigurationSection;
 
 public interface Serializable<T> {
+    /**
+     * @param section Configuration section for writing.
+     * @throws CException If writing fails.
+     */
     void write(@NonNull ConfigurationSection section) throws CException;
 
+    /**
+     * @param section Configuration section from which you need to get information
+     * @return {@code T Object}
+     * @throws CException If reading failed or some important section was not found.
+     */
     @NonNull T load(@NonNull ConfigurationSection section) throws CException;
 }
